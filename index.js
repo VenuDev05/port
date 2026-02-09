6db8c69deb2367dbfc62419208e85d3e80cc5da9
Venu <your-email@example.com>
2026-02-09 22:18:50 +0530

    Skill commit

--- index.js
+++ index.js
@@ -13,3 +13,22 @@ setInterval(() => {
   text.textContent = flow[index];
   index = (index + 1) % flow.length;
 }, 1000);
+
+const hamburger = document.getElementById("hamburger");
+const nav = document.querySelector(".nav");
+
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+const navLinks = document.querySelectorAll(".nav a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
+
+window.addEventListener('load',()=>{
+  document.body.classList.add('init')
+})
